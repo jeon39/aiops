68860cc415afb400d66cf96eb12c47d126973f31
seongj <spansite@naver.com>
2025-08-06 14:08:40 +0900

    mlflow 컨테이너 내부 path 원복

--- run_mlflow_triton.sh
+++ run_mlflow_triton.sh
@@ -8,9 +8,9 @@ echo "이 스크립트가 위치한 Dir : $ROOT_PATH"
 ##   
 docker run -it -p 8894:5000 \
 --shm-size=16G --restart always \
--v $ROOT_PATH/scripts:/mlflow/sh \
--v $ROOT_PATH/mlflow/logs:/mlflow/logs \
+-v $ROOT_PATH/mlflow/scripts:/mlflow/utils/sh \
+-v $ROOT_PATH/mlflow/logs:/mlflow/utils/logs \
 -v $ROOT_PATH/mlflow/db:/mlflow/db \
--v $ROOT_PATH/mlflow/artifacts:/artifacts \
+-v $ROOT_PATH/mlflow/artifacts:/artifacts/mlflow/models \
 --name mlflow-triton \
 spansite/aiops:mlflow-triton_v1 /bin/bash
