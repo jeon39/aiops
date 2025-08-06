9e62130ed5f36f742b218e5ec17d395b21073d13
seongj <spansite@naver.com>
2025-08-06 16:42:37 +0900

    환경변수 추가

--- run_mlflow_triton.sh
+++ run_mlflow_triton.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export MLFLOW_DB_URI="postgresql+psycopg2://genai:'genai1234!%40#$'@10.50.62.159:20159/mlflow"
+
 #ROOT_PATH=/aiops
 ROOT_PATH="$(cd "$(dirname "$0")" && pwd)"
 
@@ -10,6 +12,7 @@ echo "이 스크립트가 위치한 Dir : $ROOT_PATH"
 #   - postgresql+psycopg2://'db 유저 id':'db 비번'@'db 호스트':'db 포트번호'/'데이터 저장할 db이름'
 # 2. /artifacts/models 는 실제 서버의 디렉토리이며 대규모 아티팩트 저장을 위해 NAS로 연결되었음. 여기에 컨테이너의 /mlflow/artifacts 를 마운트하였음 
 docker run -it -p 8894:5000 --rm \
+--env MLFLOW_DB_URI=$MLFLOW_DB_URI \
 --shm-size=16G \
 -v $ROOT_PATH/mlflow/scripts:/mlflow/scripts \
 -v $ROOT_PATH/mlflow/logs:/mlflow/logs \
@@ -20,6 +23,6 @@ bash -c "\
 mlflow server \
   --host 0.0.0.0 \
   --port 5000 \
-  --backend-store-uri postgresql+psycopg2://genai:'genai1234!%40#$'@10.50.62.159:20159/mlflow \
+  --backend-store-uri \$MLFLOW_DB_URI \
   --default-artifact-root /mlflow/artifacts & \
 exec bash"
