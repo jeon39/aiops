6dbb10ef15b6ab0893317ab450824a0156f55908
seongj <spansite@naver.com>
2025-08-08 18:02:16 +0900

    릴리즈_1.0 배포

--- pipelines/test-train-project/start_project.ps1
+++ pipelines/test-train-project/start_project.ps1
@@ -1,6 +1,5 @@
 # 1. Docker build (수동 빌드)
-#docker build --no-cache --platform=linux/amd64 -t iris-classifier-image .  # 캐시 사용안하고 새로 빌드
-docker build --platform=linux/amd64 -t iris-classifier-image .   # 캐시 사용, 수정사항 없으면 기존 이미지 사용
+docker build --platform=linux/amd64 -t iris-classifier-image .
 
 # 2. Conda 환경 준비
 & "$env:USERPROFILE\anaconda3\shell\condabin\conda-hook.ps1"
@@ -11,4 +10,4 @@ $env:MLFLOW_TRACKING_URI = "http://10.50.62.185:8894"
 $env:PYTHONUTF8 = 1
 
 # 4. MLflow run (Experiment, Run Name  지정)
-mlflow run . --experiment-name "seongj-1" --run-name "random-forest-v2"
+mlflow run . --experiment-name "seongj" --run-name "random-forest-v3"
